71101af4ceb20a8e0ae832d6a4f9dd44ae3f44cd
Manoe <m.driessen@itandcare.nl>
2023-12-18 15:02:48 +0100

    regex int filter

---
+++ C#/AdventOfCode2023/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AdventOfCode2023;
+
+//Console.WriteLine("Hello, World!");
+
+var trebuchet = new Trebuchet();
+Trebuchet.Fire();
---
+++ C#/AdventOfCode2023/Trebuchet.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2023
+{
+    public class Trebuchet
+    {
+        public static void Fire()
+        {
+            Console.WriteLine("Firing trebuchet!");
+            string[] lines = File.ReadAllLines("input.txt");
+
+            foreach (var line in lines)
+            {
+                string resultString = null;
+                try
+                {
+                    var regex = new Regex(@"[^\d]");
+                    //var regex = new Regex(@"\d+");
+                    resultString = regex.Replace(line, "");
+                    Console.WriteLine(resultString);
+                }
+                catch (ArgumentException ex)
+                {
+                    // Syntax error in the regular expression
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
